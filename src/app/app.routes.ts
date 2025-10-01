5b2d21a4a20c9012e1ff10a7f71ccea30f27555e
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-10-01 00:25:42 -0600

    nav all and by id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
 
     { 
         path: 'country',
-        loadChildren: () => import('./country/country.routes'),/* .then(m => m.countryRoutes ) */
+        loadChildren: () => import('./country/country.routes'),
     },
 
     {
--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,9 @@
+import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/country-layout/country-layout.component';
-import { Routes } from '@angular/router';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 export const countryRoutes: Routes = [
 
@@ -14,17 +17,28 @@ export const countryRoutes: Routes = [
             },
 
             {
+                path: 'by-country',
+                component: ByCountryPageComponent,
+            },
+
+            {
+                path: 'by-region',
+                component: ByRegionPageComponent,
+            },
+
+            
+            {
+                path: 'by/:code',
+                component: CountryPageComponent,
+            },
+
+            
+            {
                 path: '**',
-                redirectTo: 'by-capital',
+                redirectTo: 'by-region',
             },
         ]
     },
-
-    /*{
-        path: 'country',
-        ///???{}
-    },
-*/
 
 ];
 
