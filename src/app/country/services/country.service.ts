384609f88cdf03d06861640674d7b436a04ff767
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-10-01 14:25:50 -0600

    delaySearchByCountry

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { Observable, map, catchError, throwError } from 'rxjs';
+import { Observable, map, catchError, throwError, delay } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -35,6 +35,7 @@ export class CountryService {
 
     return this.http.get<RESTCountry[]>(url).pipe(
       map((resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+      delay(2000),
       catchError(error => {
         console.log('Error fetching', error);
 
