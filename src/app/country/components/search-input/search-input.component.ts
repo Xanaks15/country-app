d0bd3684b11191806850cb703d68976eb1901162
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-10-01 17:14:19 -0600

    searchRelent

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -10,4 +10,21 @@ export class SearchInputComponent {
   placeholder = input('Buscar');
   value = output<string>();
 
+
+  inputValue = signal<string>('');
+  debounceTIme = input(300);
+
+  debounceEffect = effect((onCleanup)=>{
+    const value = this.inputValue();
+
+    const timeout = setTimeout(()=>{
+      this.value.emit(value);
+    },this.debounceTIme());
+
+    onCleanup(()=>{
+      clearTimeout(timeout);
+    });
+  })
 }
+
+
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { Observable, map, catchError, throwError, delay } from 'rxjs';
+import { Observable, map, catchError, throwError, delay, of } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
