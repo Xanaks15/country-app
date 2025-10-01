34ea1ad325c0c0c76c537630a603b6ea2a517b0f
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-10-01 14:58:24 -0600

    infoCountry

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { CountryService } from './../../services/country.service';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-country-page',
@@ -7,5 +10,13 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class CountryPageComponent {
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  CountryService = inject(CountryService)
 
+  countryResource = rxResource({
+    request: () => ({code: this.countryCode}),
+    loader: ({request}) => {
+      return this.CountryService.searchCountryByAlphaCode(request.code)
+    }
+  })
 }
